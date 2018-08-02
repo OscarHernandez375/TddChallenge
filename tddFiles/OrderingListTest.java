f8c5f134eb643e1a8e30644e8cd81b2a905031c9
Oscar Hernandez <Oscar.Hernandez@endava.com>
2018-08-02 14:48:17 -0500

    Basic test java, failed and then approved

---
+++ tddFiles/OrderingListTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
+
+public class OrderingListTest {
+
+    @Test
+    public void orderArray() {
+        OrderingList test = new OrderingList();
+
+        int[] expected = new int[20];
+        int[] unordered = new int[20];
+
+        for(int i = 0; i <20; i++){
+            expected[i] = i;
+            unordered[(i*11+7)%20]=i;
+        }
+
+        int[] real = expected;
+
+
+        assertArrayEquals(expected,real);
+    }
+}
