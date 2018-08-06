d163694ee9da4c6f48cc5e194dff2034d51ca827
Oscar Hernandez <Oscar.Hernandez@endava.com>
2018-08-06 12:31:17 -0500

    Optimized ordering code

--- tddFiles/OrderingList.java
+++ tddFiles/OrderingList.java
@@ -1,19 +1,30 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class OrderingList {
     public int[] orderArray(int[] array){
-		if (array == null){
-			return null;
-		}
-        for (int i =0; i< array.length; i++){
-            for (int j =0; j< array.length; j++){
-                if(j<i && array[j] > array [i]){
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
-            }
+        if(array == null){
+            return null;
         }
-        return array;
+        int max = Arrays.stream(array).max().getAsInt();
+        int min = Arrays.stream(array).min().getAsInt();
+
+        int[] count = new int[max-min+1];
+        int[] output = new int[array.length];
+        for (int x: array){
+            ++count[x-min];
+        }
+        for (int j = 1; j <= (max-min);j++){
+            count[j]+=count[j-1];
+        }
+
+        for (int x: array){
+            output[count[x-min]-1] = x;
+            --count[x-min];
+        }
+
+        return output;
     }
+
 }
