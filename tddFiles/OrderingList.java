99b4dba52d62a79235ae59221210f7b8fbdcc3de
Oscar Hernandez <Oscar.Hernandez@endava.com>
2018-08-06 10:08:55 -0500

    Modified code to accept null

--- tddFiles/OrderingList.java
+++ tddFiles/OrderingList.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class OrderingList {
     public int[] orderArray(int[] array){
+		if (array == null){
+			return null;
+		}
         for (int i =0; i< array.length; i++){
             for (int j =0; j< array.length; j++){
                 if(j<i && array[j] > array [i]){
